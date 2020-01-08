46d0b1c53c55423259b5f82d3ba451db4e13df10
gujos110 <jlgo1968@gmail.com>
2020-01-08 11:37:34 -0400

    It was added new line from desktop at 11:28

--- ex0.py
+++ ex0.py
@@ -1,9 +1,25 @@
-print('Hello from GitHub')
-print('It works from desktop')
-print('I received your new line from GitHub')
-print('This is from HP notebook')
+def one():
+    print('It comes to one function')
+    return 'January'
 
-print('I received all your modifications from desktop')
+def two():
+    return 'February'
 
-print('Thanks I already received all, from HP Notebook')
+def three():
+    return 'March'
+
+
+def numbers_to_months(argument):
+    switcher = {
+        1: one,
+        2: two,
+        3: three
+    }
+    # get the function from switcher dictionary
+    func = switcher.get(argument, lambda: 'Invalid month')
+    # execute the function
+    print(func())
+
+
+numbers_to_months(2)
 
